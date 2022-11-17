b57bcb1ab62d20d49d3b86a9355553217c89e3df
psyduck <1241280257@qq.com>
2022-11-17 19:02:17 +0900

    init

---
+++ notebook-admin/src/main/java/com/psyduck/notebook/web/controller/system/SysIndexController.java
@@ -0,0 +1,29 @@
+package com.psyduck.notebook.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.psyduck.notebook.common.config.PsyDuckNoteBookConfig;
+import com.psyduck.notebook.common.utils.StringUtils;
+
+/**
+ * 首页
+ *
+ * @author ruoyi
+ */
+@RestController
+public class SysIndexController
+{
+    /** 系统基础配置 */
+    @Autowired
+    private PsyDuckNoteBookConfig psyDuckNoteBookConfig;
+
+    /**
+     * 访问首页，提示语
+     */
+    @RequestMapping("/")
+    public String index()
+    {
+        return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", psyDuckNoteBookConfig.getName(), psyDuckNoteBookConfig.getVersion());
+    }
+}
---
+++ notebook-admin/src/main/java/com/psyduck/notebook/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.psyduck.notebook.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.psyduck.notebook.common.core.controller.BaseController;
+import com.psyduck.notebook.common.core.domain.AjaxResult;
+import com.psyduck.notebook.common.core.domain.model.RegisterBody;
+import com.psyduck.notebook.common.utils.StringUtils;
+import com.psyduck.notebook.framework.web.service.SysRegisterService;
+import com.psyduck.notebook.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
